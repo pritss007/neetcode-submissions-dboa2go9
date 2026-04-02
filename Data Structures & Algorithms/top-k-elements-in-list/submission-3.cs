c4cc7989c4cc0513e20b3e900ffeff6dd109ef0a
pritamkumar jadhav <55727765+pritss007@users.noreply.github.com>
2026-04-03 00:11:58 +0530

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+        Dictionary<int, int> map = new();
+        
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(!map.TryGetValue(nums[i], out int value))
+            {
+                map[nums[i]] = 1;
+            }
+            else{
+                map[nums[i]]++;
+            }
+        }
+
+        var orderedDic = map.OrderByDescending(k => k.Value);
+
+        int[] result = new int[k];
+         foreach(var car in orderedDic){
+            if(k > 0)
+        {
+           result[k-1] = car.Key;
+                k--;
+        }
+        else{
+            break;
+        }
+                
+            }
+        
+        return result;
+    }
+}
