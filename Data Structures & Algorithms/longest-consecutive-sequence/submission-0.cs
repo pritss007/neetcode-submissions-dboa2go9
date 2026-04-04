6d0cc7bb7c5c9ac081483fbaa0265f06dc8277ac
pritamkumar jadhav <55727765+pritss007@users.noreply.github.com>
2026-04-04 22:28:04 +0530

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+         HashSet<int> set = new HashSet<int>(nums);
+    int longest = 0;
+
+    foreach(int num in set)
+    {
+        // start of sequence
+        if(!set.Contains(num - 1))
+        {
+            int current = num;
+            int length = 1;
+
+            while(set.Contains(current + 1))
+            {
+                current++;
+                length++;
+            }
+
+            longest = Math.Max(longest, length);
+        }
+    }
+
+    return longest;
+
+
+    }
+}
